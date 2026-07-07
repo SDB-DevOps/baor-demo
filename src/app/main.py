ad4e5a88deaa16eb2af8db11638082a5c3ebc4c6
Rui Bao <Rui.BR.Bao@ibm.com>
2026-07-07 10:21:55 +0800

    update main files

--- src/app/main.py
+++ src/app/main.py
@@ -7,7 +7,10 @@ from app.calculator import add
 
 def main() -> None:
     result = add(2, 3)
+    print("++++++++++++++++++")
     print(f"2 + 3 = {result}")
+    print("++++++++++++++++++")
+
 
 
 if __name__ == "__main__":
